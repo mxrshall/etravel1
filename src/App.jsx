ff49bc93a4c483d3314082c237aaae76c2c9faa6
Martin Krajčovič <martin22.krajcovic@gmail.com>
2025-02-06 00:51:18 +0100

    framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { motion } from "framer-motion"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-400">
+    <motion.h1
+      animate={{ x: 100 }} 
+      transition={{ duration: 0.5 }}
+      className="text-3xl font-bold underline text-red-400">
       Hello world!
-    </h1>
+    </motion.h1>
   )
 }
